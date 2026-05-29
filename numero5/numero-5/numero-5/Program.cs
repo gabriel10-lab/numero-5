eec6e9165b5dca3d461274454b273cd66ad87ee0
gabriel10-lab <red2003ho@outlook.com>
2026-05-28 22:42:21 -0300

    Add files via upload

---
+++ numero5/numero-5/numero-5/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("---FLuxograma da engenharia ---");
+
+Console.WriteLine("esta se omvendo (s/n)");
+Consolekey pressione=Console.Readkey(true).key;
+if (pressione==Consolekey.s)
+
+    Consolekey pressione=Console.Readkey(true.key);
+    
+    if (pressione==Consolekey.S)
+
+
+  
+Console.WriteLine("E isso ai!");
+
+
+if (pressione==Consolekey.N)
+{
+    Console.WriteLine("opcao invalida");
+}
+
+
+else if (pressione==Consolekey.N)
+{
+    Console.WriteLine("e isso ai");
+}
+
+else if (pressione==Consolekey.S)
+{
+    Console.WriteLine("Use WD-40");
+}
+else if
+{
+    Console.WriteLine("opçao invalida");
+}   
+else
+{
+    Console.WriteLine("opcao invaliad");
+}
+
