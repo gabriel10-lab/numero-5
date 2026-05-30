beb1f000bc342cf3b6abae1cf1942f64612a641c
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2026-05-29 21:55:35 -0300

    mudanca

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿while(true)
+{    
+    Console.WriteLine("--- Fluxograma da engenharia ---");
+
+    Console.WriteLine("esta se movendo? (S/N)");
+
+    ConsoleKey pressione = Console.ReadKey(true).Key;
+
+    if (pressione == ConsoleKey.S)
+    {
+        Console.WriteLine("deveria?");
+        ConsoleKey pressione2 = Console.ReadKey(true).Key;
+
+        if (pressione2 == ConsoleKey.S)
+        {
+            Console.WriteLine("é isso ai!");
+        }
+        else if (pressione2 == ConsoleKey.N)
+        {
+            Console.WriteLine("use silver");
+        }
+        else
+        {
+            Console.WriteLine("opcao invalida");
+        }
+    }
+    else if (pressione == ConsoleKey.N)
+    {
+        Console.WriteLine("deveria?");
+        ConsoleKey pressione2 = Console.ReadKey(true).Key;
+
+        if (pressione2 == ConsoleKey.S)
+        {
+            Console.WriteLine("use WD-40");
+        }
+        else if (pressione2 == ConsoleKey.N)
+        {
+            Console.WriteLine("é isso ai!");
+        }
+        else
+        {
+            Console.WriteLine("opcao invalida");
+        }
+    }
+    else
+    {
+        Console.WriteLine("opcao invalida");
+    }
+}
